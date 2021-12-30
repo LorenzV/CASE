c71d673c28d31652df15ad571a99c759065b6743
LorenzV <lorenz.voegeli@gmx.ch>
2021-12-30 16:47:53 +0100

    new hmtl and css uebung und math

--- smartprep/prep/urls.py
+++ smartprep/prep/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home ), #our-domain.com/prep
-    path('fach/', views.fach ) #our-domain.com/prep
+    path('math/', views.math ), #our-domain.com/prep
+    path('uebung/', views.uebung ) #our-domain.com/prep
 ]
--- smartprep/prep/views.py
+++ smartprep/prep/views.py
@@ -11,10 +11,37 @@ def home(request):
     return render(request, 'prep/home.html', {
     })
 
-def fach(request):
+
+def math(request):
+    uebung = [
+        {
+            'titel': 'Test',
+            'id': 1,
+            'fach': 'Mathematik'      
+        },
+        {
+            'titel': 'Test 3',
+            'id': 12,
+            'fach': 'Mathematik'      
+    }]
     
     
-    return render(request, 'prep/fach.html', {
+    return render(request, 'prep/math.html', {
+        'show_uebung': True,
+        'uebung':uebung
+    })
+
+def uebung(request):
+    selected_uebung ={
+            'titel': 'Übung 1',
+            'frage':'Testfrage ist Testfrage...',
+            'antwort':'isch richtig!',
+            'fach': 'Mathematik'      
+        }
+    return render(request,'prep/uebung.html',{
+        'uebung_titel': selected_uebung['titel'],
+        'uebung_frage': selected_uebung['frage'],
+        'uebung_antwort': selected_uebung['antwort']
     })
 
 
