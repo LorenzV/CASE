7aa53f7bfb8a049f5d2ed915d67abb486d09d1f8
LorenzV <lorenz.voegeli@gmx.ch>
2021-12-30 18:35:00 +0100

    staarch

--- smartprep/prep/urls.py
+++ smartprep/prep/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home ), #our-domain.com/prep
-    path('math/', views.math ), #our-domain.com/prep
-    path('uebung/', views.uebung ) #our-domain.com/prep
-]
+    path('home/', views.home, name='home' ), #our-domain.com/prep
+    path('math/', views.math, name='math'), #our-domain.com/prep
+    path('math/<slug:uebung_slug>/', views.uebung, name='uebung') #our-domain.com/"username"/uebungxxx
+    ]
--- smartprep/prep/views.py
+++ smartprep/prep/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-from prep.models import Uebung
-
+from .models import Uebung
 
 # Create your views here.
 
@@ -13,36 +12,23 @@ def home(request):
 
 
 def math(request):
-    uebung = [
-        {
-            'titel': 'Test',
-            'id': 1,
-            'fach': 'Mathematik'      
-        },
-        {
-            'titel': 'Test 3',
-            'id': 12,
-            'fach': 'Mathematik'      
-    }]
-    
+    uebung = Uebung.objects.all()
     
     return render(request, 'prep/math.html', {
         'show_uebung': True,
         'uebung':uebung
     })
 
-def uebung(request):
-    selected_uebung ={
-            'titel': 'Übung 1',
-            'frage':'Testfrage ist Testfrage...',
-            'antwort':'isch richtig!',
-            'fach': 'Mathematik'      
-        }
+def uebung(request, uebung_slug):
+    selected_uebung = Uebung.objects.get(slug=uebung_slug)
+        
     return render(request,'prep/uebung.html',{
-        'uebung_titel': selected_uebung['titel'],
-        'uebung_frage': selected_uebung['frage'],
-        'uebung_antwort': selected_uebung['antwort']
+        'uebung_titel': selected_uebung.titel,
+        'uebung_frage': selected_uebung.frage,
+        'uebung_antwort': selected_uebung.antwort
     })
+
+
 
 
 
